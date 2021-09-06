405b2a37f517209ba7cbc00342a112ce91e4388a
ccaiomacedo <caio.macedo@ucsal.edu.br>
2021-09-06 19:18:50 -0300

    Static charts

--- frontend/src/components/NavBar/index.tsx
+++ frontend/src/components/NavBar/index.tsx
@@ -1,5 +1,4 @@
 import ImgDsDark from 'assets/img/ds-dark.svg'
-
 const NavBar = () =>{
     return (
         <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-light border-bottom shadow-sm">
