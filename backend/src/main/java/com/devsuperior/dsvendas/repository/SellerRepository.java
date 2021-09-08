765b889251d1b9ccc18e706992d65dff5114c0e6
ccaiomacedo <caio.macedo@ucsal.edu.br>
2021-09-08 13:20:14 -0300

    Layers

---
+++ backend/src/main/java/com/devsuperior/dsvendas/repository/SellerRepository.java
@@ -0,0 +1,7 @@
+package com.devsuperior.dsvendas.repository;
+
+import com.devsuperior.dsvendas.entities.Seller;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SellerRepository extends JpaRepository<Seller,Long> {
+}
