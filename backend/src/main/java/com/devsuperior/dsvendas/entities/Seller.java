d3db5f7e5684c173f3507d6a935f5cdfe4a59c82
ccaiomacedo <caio.macedo@ucsal.edu.br>
2021-09-08 16:48:05 -0300

    First homolog

--- backend/src/main/java/com/devsuperior/dsvendas/entities/Seller.java
+++ backend/src/main/java/com/devsuperior/dsvendas/entities/Seller.java
@@ -13,7 +13,7 @@ public class Seller {
     private String name;
 
     @OneToMany(mappedBy = "seller")
-    private List<Sale> sales = new ArrayList<>();
+    private final List<Sale> sales = new ArrayList<>();
 
     public Seller(){
 
