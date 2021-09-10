c7a87a0127c272f78925bb92e75b5d8532ef0825
ccaiomacedo <caio.macedo@ucsal.edu.br>
2021-09-10 16:51:49 -0300

    Home, navigation

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -6,7 +6,7 @@ const Footer = () =>{
                 <p className="text-light"><small><strong>Semana Spring React</strong><br/>
                 Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
             </div>
-      </footer>
+        </footer>
     );
   }
   
